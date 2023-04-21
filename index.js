c3ba9f92ab22b814dc4764fc16977fa1de3e235d
Ox Noj <geneveva@gmail.com>
2023-04-20 18:45:09 -0700

    license information

--- index.js
+++ index.js
@@ -6,18 +6,29 @@ const path = require('path') //a built in method, platform path.
 // If there is no license, return an empty string
 // A short list of 4
 //case sensitive
-function renderLicenseBadge(license) { 
+function renderLicenseBadge(license) {
+    // if (license === 'MIT') {
+    //     return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;//link to direct MIT logo
+    // } else if (license === 'Apache') {
+    //     return `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
+    // } else if (license === 'Mozilla') {
+    //     return `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0`;
+    // } else {
+    //     return `![License](https://img.shields.io/badge/License-EPL%201.0-red.svg)](https://opensource.org/licenses/EPL-1.0)`;
+    // }
     if (license === 'MIT') {
-        return `https://shields.io/badge/license-MIT-green`//link to direct MIT logo
-    }   else if (license === 'Apache') {
-
-        return `https://shields.io/badge/license-Apache-blue`
+        return
     }
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
+    if (license === 'none') {
+        return "";
+    } else {
+        return `https://opensource.org/licenses/${license}`;
+    }
 }
 
 // TODO: Create a function that returns the license section of README
@@ -54,7 +65,7 @@ function generateMarkdown(response) {
   
 ## License 
     ${response.license}`
-  };
+};
 
 const questions =
     [
@@ -89,15 +100,14 @@ const questions =
             message: 'An area to list tech, collaborators, 3rd party assets.',
         },
         {
-            type: 'checkbox', //type list, to give choices
+            type: 'list', //type list, to give choices
             name: 'license',
             message: 'Which license are you using?',
-            choices: [
-                'MIT', 'Apache', 'Mozilla', 'Eclipse',
-            ],
+            choices: ['MIT', 'Apache', 'Mozilla', 'Eclipse'],
+            default: 'MIT',
         },
         {
-            type: 'list', 
+            type: 'input',
             name: 'status',
             message: 'Project Status',
         },
@@ -111,17 +121,17 @@ const questions =
 
 function writeToFile(fileName, response) {
     return fs.writeFileSync(path.join(process.cwd(), fileName), response);
-  }
-    
+}
+
 
 // TODO: Create a function to initialize app
 function init() {
     inquirer.prompt(questions).then((inquirerResponses) => {
-      console.log('Generating README...');
-      writeToFile('README2.md', generateMarkdown({ ...inquirerResponses }));
+        console.log('Generating README...');
+        writeToFile('README2.md', generateMarkdown({ ...inquirerResponses }));
     });
-  }
-  
+}
+
 
 // Function call to initialize app
 init();
